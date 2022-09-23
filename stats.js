6d8d888fbf26dba2d41a78db9308505a9b89abc4
0xlz5 <federico.cagliero@gmail.com>
2022-09-23 07:36:55 +0200

    Update stats.js

--- stats.js
+++ stats.js
@@ -3,62 +3,62 @@
 */
 
 Object.prototype.print = function(output) {
-	Object.entries(this).forEach(
-		([k, value]) => { output.log(k, " - ", value); }
-	);
+  Object.entries(this).forEach(
+    ([k, value]) => { output.log(k, " - ", value); }
+  );
 }
 
 class Digits {
-	static __SEQ = '0123456789abcedfghijklmopqrstuvwyz';
+  static __SEQ = '0123456789abcedfghijklmopqrstuvwyz';
 
-	constructor(radix) {
-		this.__radix = radix;
-		this.__digits = Digits.__SEQ.substring(0, radix);
-		this.setUp();
-	}
+  constructor(radix) {
+    this.__radix = radix;
+    this.__digits = Digits.__SEQ.substring(0, radix);
+    this.setUp();
+  }
 
-	setUp() {
-		this.__t = new Map(
-			this.__digits.split('')
-				.map(
-					(i) => { return [ parseInt(i, this.__radix), i ] }
-				)
-		);
-		this.__stats = {};
-		for (const d of this.__digits) {
-			this.__stats[d] = 0;
-		}
-	}
+  setUp() {
+    this.__t = new Map(
+      this.__digits.split('')
+        .map(
+          (i) => { return [ parseInt(i, this.__radix), i ] }
+        )
+    );
+    this.__stats = {};
+    for (const d of this.__digits) {
+      this.__stats[d] = 0;
+    }
+  }
 
-	update(n) {
-		this.__stats[n]++;
-	}
+  update(n) {
+    this.__stats[n]++;
+  }
 
-	getStats() {
-		return this.__stats;
-	}
+  getStats() {
+    return this.__stats;
+  }
 }
 
 class Binary extends Digits {
-	constructor() {
-		super(2);
-	}
+  constructor() {
+    super(2);
+  }
 
-	__bits(n) {
-		let b = '';
-		for (let i = n; ; i = i >> 1) {
-			b = this.__t.get(i % 2) + b;
-			if (i <= 0) break;
-		}
-		return b;
-	}
+  __bits(n) {
+    let b = '';
+    for (let i = n; ; i = i >> 1) {
+      b = this.__t.get(i % 2) + b;
+      if (i <= 0) break;
+    }
+    return b;
+  }
 
-	computeStats(n) {
-		for (const b of this.__bits(n)) {
-			this.update(b);
-		}
-		return this.getStats();
-	}
+  computeStats(n) {
+    for (const b of this.__bits(n)) {
+      this.update(b);
+    }
+    return this.getStats();
+  }
 }
 
 let obj = new Binary();
